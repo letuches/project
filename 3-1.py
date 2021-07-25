33a9fb42e38e1e2865e40dab427c00637382816a
Dmitry <letuches@mail.ru>
2021-07-25 11:42:29 +0300

    task 3

--- 3-1.py
+++ 3-1.py
@@ -1,11 +1,13 @@
 # task 1
 
-f_val = float(input("Первое число: "))
-s_val = float(input("Второе число: "))
-if s_val == 0:
-   print("Ошибка ввода, деление на 0 невозможно!!!")
-else:
-    def func(f_val, s_val):
-        return f_val / s_val
-    print(func(f_val, s_val))
+val1 = float(input("Первое число: "))
+val2 = float(input("Второе число: "))
+if val2 == 0:
 
+    print("Ошибка ввода, деление на 0 невозможно!!!")
+else:
+    def func(val1, val2):
+        return val1 / val2
+
+
+    print(func(val1, val2))
