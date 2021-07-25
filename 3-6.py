d4b7debd11480dce00434cbefafeda6a041216ce
Dmitry <letuches@mail.ru>
2021-07-25 15:41:59 +0300

    task 3

--- 3-6.py
+++ 3-6.py
@@ -1,1 +1,12 @@
 # task 3-6
+
+def int_func(text):
+    word = text[0].upper() + text[1:].lower()
+    return word
+
+
+print(int_func('proba'))
+
+words = input('Enter some words: ')
+for word in words.split(' '):
+    print(f'{int_func(word)}', end=' ')
